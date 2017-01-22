10a96395f5fcf7398f1357254fde10b497e272d1
ahom <antoine.hom@gmail.com>
2017-01-22 14:35:20 -0500

    luigi

--- mediaserver/tasks.py
+++ mediaserver/tasks.py
@@ -2,9 +2,10 @@ from pathlib import Path
 import json
 from collections import namedtuple
 from itertools import chain
+import uuid
+import os
 
-from taskflow import task
-from taskflow.patterns import linear_flow as lf
+import luigi
 
 from ffmpy import FFprobe, FFmpeg
 
@@ -15,16 +16,12 @@ Streams = namedtuple('Streams', ['videos', 'audios', 'subtitles'])
 FileFormat = namedtuple('FileFormat', ['filename', 'tags'])
 FileInfo = namedtuple('FileInfo', ['file_format', 'streams'])
 
-class GetFFprobeOutput(task.Task):
-    default_provides = 'ffprobe_output'
+class GetFFprobeOutputTask(luigi.Task):
+    file_path = luigi.Parameter()
 
-    def __init__(self, ffprobe_func=FFprobe):
-        super(GetFFprobeOutput, self).__init__()
-        self.ffprobe_func = ffprobe_func
-
-    def execute(self, file_path):
+    def run(self):
         ff = self.ffprobe_func(
-            inputs={ file_path.as_posix(): None },
+            inputs={ file_work_path.as_posix(): None },
             global_options=[
                 '-v', 'error',
                 '-print_format', 'json',
@@ -34,75 +31,77 @@ class GetFFprobeOutput(task.Task):
 
         out, err = ff.run()
 
-        return out
+        with self.output().open('w') as output_file:
+            output_file.write(out)
 
-class DecodeFFprobeOutput(task.Task):
-    default_provides = 'file_info'
+    def output(self):
+        return luigi.LocalTarget('%s.ffprobe' % self.file_path)
 
-    def execute(self, ffprobe_output):
-        j = json.loads(ffprobe_output)
-        streams = [ o for o in j['streams'] if 'codec_type' in o ]
 
-        return FileInfo(
-            file_format=FileFormat(filename=j['format']['filename'],tags=j['format']['tags']),
-            streams=Streams(
-                videos=[
-                    VideoStream(
-                        index=o['index'],
-                        codec=o['codec_name'],
-                        width=o['width'],
-                        height=o['height']
+class DecodeFFprobeOutputTask(luigi.Task):
+    file_path = luigi.Parameter()
+
+    def requires(self):
+        return GetFFprobeOutputTask(self.file_path)
+
+    def run(self):
+        with self.input().open('r') as ffprobe_input_file:
+            j = json.load(ffprobe_input_file)
+            streams = [ o for o in j['streams'] if 'codec_type' in o ]
+
+            with self.output().open('w') as output_file:
+                pickle.dumps(FileInfo(
+                    file_format=FileFormat(filename=j['format']['filename'],tags=j['format']['tags']),
+                    streams=Streams(
+                        videos=[
+                            VideoStream(
+                                index=o['index'],
+                                codec=o['codec_name'],
+                                width=o['width'],
+                                height=o['height']
+                            )
+                            for o in streams if o['codec_type'] == 'video'
+                        ],
+                        audios=[
+                            AudioStream(
+                                index=o['index'],
+                                codec=o['codec_name'],
+                                lang=o.get('tags', {}).get('language', None)
+                            )
+                            for o in streams if o['codec_type'] == 'audio'
+                        ],
+                        subtitles=[
+                            SubtitleStream(
+                                index=o['index'],
+                                lang=o.get('tags', {}).get('language', None)
+                            )
+                            for o in streams if o['codec_type'] == 'subtitle'
+                        ]
                     )
-                    for o in streams if o['codec_type'] == 'video'
-                ],
-                audios=[
-                    AudioStream(
-                        index=o['index'],
-                        codec=o['codec_name'],
-                        lang=o.get('tags', {}).get('language', None)
-                    )
-                    for o in streams if o['codec_type'] == 'audio'
-                ],
-                subtitles=[
-                    SubtitleStream(
-                        index=o['index'],
-                        lang=o.get('tags', {}).get('language', None)
-                    )
-                    for o in streams if o['codec_type'] == 'subtitle'
-                ]
-            )
-        )
+                ))
 
-def GetFileInfo():
-    f = lf.Flow()
-    f.add(GetFFprobeOutput())
-    f.add(DecodeFFprobeOutput())
-    return f
-
-class FetchTMDbIds(task.Task):
-    default_provides = 'tmdb_ids'
-
-    # TODO: fetch TMDbIds from file_info and torrent_name
-    def execute(self, file_info, torrent_name):
-        return []
+    def output(self):
+        return luigi.LocalTarget('%s.info' % self.file_path)
 
 class EncodeVideoStream(task.Task):
+    default_provides = 'video_encode_file_path'
+
     DEFINITION_TO_BITRATE = {
         1080: '6M',
         720:  '3M',
         480:  '1.5M'
     }
 
-    def __init__(self, definition, ffmpeg_func=FFmpeg):
-        super(EncodeVideoStream, self).__init__()
+    def __init__(self, definition, ffmpeg_func=FFmpeg, **kwargs):
+        super(EncodeVideoStream, self).__init__(**kwargs)
         self.definition = definition
         self.ffmpeg_func = ffmpeg_func
 
-    def _first_pass(self, file_path, common_options):
+    def _first_pass(self, file_work_path, common_options):
         ff = self.ffmpeg_func(
-            inputs={ file_path.as_posix(): None },
+            inputs={ file_work_path.as_posix(): None },
             outputs={ 
-                'dummy_first_pass.webm': chain(common_options, [
+                '/dev/null': chain(common_options, [
                     '-pass', '1',
                     '-speed', '4'
                 ])
@@ -111,11 +110,11 @@ class EncodeVideoStream(task.Task):
 
         ff.run()
 
-    def _second_pass(self, file_path, common_options):
-        output_file_path = file_path.parent / 'video-%s.webm' % self.definition
+    def _second_pass(self, file_work_path, common_options):
+        output_file_path = file_work_path.parent / ('video-%sp.webm' % self.definition)
 
         ff = self.ffmpeg_func(
-            inputs={ file_path.as_posix(): None },
+            inputs={ file_work_path.as_posix(): None },
             outputs={ 
                 output_file_path.as_posix(): chain(common_options, [
                     '-pass', '2',
@@ -130,34 +129,40 @@ class EncodeVideoStream(task.Task):
 
         return output_file_path
 
-    def execute(self, file_path, video_stream):
-        common_options = [
-            '-map', '0:%s' % video_stream.index,
-            '-c:v', 'libvpx-vp9',
-            '-s', '%sx%s' % (self.definition * video_stream.width // video_stream.height, self.definition),
-            '-b:v', DEFINITION_TO_BITRATE[self.definition],
-            '-tile-columns', '6',
-            '-frame-parallel', '1',
-            '-keyint_min', '150',
-            '-g', '150',
-            '-qmin', '0',
-            '-qmax', '50',
-            '-f', 'webm',
-            '-dash', '1'
-        ]
-        self._first_pass(file_path, common_options)
-        return self._second_pass(file_path, common_options)
+    def execute(self, file_work_path, video_stream):
+        if self.definition > video_stream.height:
+            # We do not encode a video with a definition > than the source
+            return None
+        else:
+            common_options = [
+                '-map', '0:%s' % video_stream.index,
+                '-c:v', 'libvpx-vp9',
+                '-s', '%sx%s' % (self.definition * video_stream.width // video_stream.height, self.definition),
+                '-b:v', self.DEFINITION_TO_BITRATE[self.definition],
+                '-tile-columns', '6',
+                '-frame-parallel', '1',
+                '-keyint_min', '150',
+                '-g', '150',
+                '-qmin', '0',
+                '-qmax', '50',
+                '-f', 'webm',
+                '-dash', '1'
+            ]
+            self._first_pass(file_work_path, common_options)
+            return self._second_pass(file_work_path, common_options)
 
 class EncodeAudioStream(task.Task):
-    def __init__(self, ffmpeg_func=FFmpeg):
-        super(EncodeAudioStream, self).__init__()
+    default_provides = 'audio_encode_file_path'
+
+    def __init__(self, ffmpeg_func=FFmpeg, **kwargs):
+        super(EncodeAudioStream, self).__init__(**kwargs)
         self.ffmpeg_func = ffmpeg_func
 
-    def execute(self, file_path, audio_stream):
-        output_file_path = file_path.parent / 'audio-%s.webm' % audio_stream.lang
+    def execute(self, file_work_path, audio_stream):
+        output_file_path = file_work_path.parent / ('audio-%s.webm' % audio_stream.lang)
 
         ff = self.ffmpeg_func(
-            inputs={ file_path.as_posix(): None },
+            inputs={ file_work_path.as_posix(): None },
             outputs={ 
                 output_file_path.as_posix(): [
                     '-map', '0:%s' % audio_stream.index,
@@ -174,15 +179,17 @@ class EncodeAudioStream(task.Task):
         return output_file_path
 
 class EncodeSubtitleStream(task.Task):
-    def __init__(self, ffmpeg_func=FFmpeg):
-        super(EncodeSubtitleStream, self).__init__()
+    default_provides = 'subtitle_encode_file_path'
+
+    def __init__(self, ffmpeg_func=FFmpeg, **kwargs):
+        super(EncodeSubtitleStream, self).__init__(**kwargs)
         self.ffmpeg_func = ffmpeg_func
 
-    def execute(self, file_path, subtitle_stream):
-        output_file_path = file_path.parent / 'subtitle-%s.vtt' % subtitle_stream.lang
+    def execute(self, file_work_path, subtitle_stream):
+        output_file_path = file_work_path.parent / ('subtitle-%s.vtt' % subtitle_stream.lang)
 
         ff = self.ffmpeg_func(
-            inputs={ file_path.as_posix(): None },
+            inputs={ file_work_path.as_posix(): None },
             outputs={ 
                 output_file_path.as_posix(): [
                     '-map', '0:%s' % subtitle_stream.index
@@ -193,6 +200,29 @@ class EncodeSubtitleStream(task.Task):
         ff.run()
 
         return output_file_path
+
+def CreateWorkDir(task.Task):
+    default_provides = 'work_dir'
+
+    def __init__(self, parent_path, **kwargs):
+        super(CreateWorkDir, self).__init__(**kwargs)
+        self.parent_path = parent_path
+
+    def execute(self):
+        work_dir = parent_path / uuid.uuid4().hex
+        os.makedirs(work_dir)
+        return work_dir
+
+def ProcessFile(file_path):
+    p = Path('~/temp_dirs')
+    f = lf.Flow()
+    f.add(CreateWorkDir(p))
+    f.add(ExplodePath(rebind={ 'path': 'file_path' }, provides=('file_path_dir', 'file_path_name')))
+    f.add(JoinPaths(rebind={ 'path1': 'work_dir', 'path2': 'file_path_name' }, provides=('file_work_path'))
+    f.add(CopyFile(rebind={ 'source': 'file_path', 'dest': 'file_work_path' }))
+    f.add(GetFFprobeOutput())
+    f.add(DecodeFFprobeOutput())
+
 
 # Process torrent (Launched by web ui)
 #  - Fetch torrent file
--- mediaserver/test_tasks.py
+++ mediaserver/test_tasks.py
@@ -4,66 +4,85 @@ from itertools import chain
 
 from ffmpy import FFmpeg, FFprobe
 
-from tasks import GetFFprobeOutput, DecodeFFprobeOutput, FileInfo, VideoStream, AudioStream, SubtitleStream
+from tasks import *
 
-def ffmpy_cmd_out(cmd):
+def ffmpy_cmd_out(cmd): 
+    calls = []
     class WrappedFFmpyCmd:
         def __init__(self, *args, **kwargs):
             self.cmd = cmd(*args, **kwargs)
 
         def run(self):
-            return self.cmd.cmd, None
-    return WrappedFFmpyCmd
-
-FFprobeCmdOut = ffmpy_cmd_out(FFprobe)
-FFmpegCmdOut = ffmpy_cmd_out(FFmpeg)
+            calls.append(self.cmd.cmd)
+            return None, None
+    return WrappedFFmpyCmd, calls
 
 def test_GetFFprobeOutput():
-    assert "ffprobe -v error -print_format json -show_streams -i input.mkv" == GetFFprobeOutput(FFprobeCmdOut).execute(Path("input.mkv"))
+    wrapper, calls = ffmpy_cmd_out(FFprobe)
+    GetFFprobeOutput(wrapper).execute(Path('input.mkv'))
+    assert calls == ['ffprobe -v error -print_format json -show_streams -i input.mkv']
 
 def test_DecodeFFprobeOutput():
-    filename = "input.mkv"
+    filename = 'input.mkv'
     tags = {
-        "title": "title"
+        'title': 'title'
     }
     video_streams = [
-        { "index": 0, "codec_type": "video", "codec_name": "h264", "width": 1920, "height": 1080 }
+        { 'index': 0, 'codec_type': 'video', 'codec_name': 'h264', 'width': 1920, 'height': 1080 }
     ]
     audio_streams = [
-        { "index": 1, "codec_type": "audio", "codec_name": "dts", "tags": { "language": "eng" } },
-        { "index": 2, "codec_type": "audio", "codec_name": "vorbis", "tags": {} },
-        { "index": 3, "codec_type": "audio", "codec_name": "opus" }
+        { 'index': 1, 'codec_type': 'audio', 'codec_name': 'dts', 'tags': { 'language': 'eng' } },
+        { 'index': 2, 'codec_type': 'audio', 'codec_name': 'vorbis', 'tags': {} },
+        { 'index': 3, 'codec_type': 'audio', 'codec_name': 'opus' }
     ]
     subtitle_streams = [
-        { "index": 4, "codec_type": "subtitle", "tags": { "language": "eng" } },
-        { "index": 5, "codec_type": "subtitle", "tags": { "language": "fre" } },
-        { "index": 6, "codec_type": "subtitle", "tags": { } },
-        { "index": 7, "codec_type": "subtitle" }
+        { 'index': 4, 'codec_type': 'subtitle', 'tags': { 'language': 'eng' } },
+        { 'index': 5, 'codec_type': 'subtitle', 'tags': { 'language': 'fre' } },
+        { 'index': 6, 'codec_type': 'subtitle', 'tags': { } },
+        { 'index': 7, 'codec_type': 'subtitle' }
     ]
     misc_streams = [
-        { "codec_type": "misc" },
+        { 'codec_type': 'misc' },
         {}
     ]
     fi = DecodeFFprobeOutput().execute(json.dumps({
-        "format": {
-            "filename": filename,
-            "tags": tags
+        'format': {
+            'filename': filename,
+            'tags': tags
         },
-        "streams": list(chain(video_streams, audio_streams, subtitle_streams, misc_streams)) 
+        'streams': list(chain(video_streams, audio_streams, subtitle_streams, misc_streams)) 
     }))
     assert fi.file_format.filename == filename
     assert fi.file_format.tags == tags
     assert fi.streams.videos == [
-        VideoStream(index=0, codec="h264", width=1920, height=1080)
+        VideoStream(index=0, codec='h264', width=1920, height=1080)
     ]
     assert fi.streams.audios == [
-        AudioStream(index=1, codec="dts", lang="eng"),
-        AudioStream(index=2, codec="vorbis", lang=None),
-        AudioStream(index=3, codec="opus", lang=None)
+        AudioStream(index=1, codec='dts', lang='eng'),
+        AudioStream(index=2, codec='vorbis', lang=None),
+        AudioStream(index=3, codec='opus', lang=None)
     ]
     assert fi.streams.subtitles == [
-        SubtitleStream(index=4, lang="eng"),
-        SubtitleStream(index=5, lang="fre"),
+        SubtitleStream(index=4, lang='eng'),
+        SubtitleStream(index=5, lang='fre'),
         SubtitleStream(index=6, lang=None),
         SubtitleStream(index=7, lang=None)
+    ]
+
+def test_EncodeSubtitleStream():
+    wrapper, calls = ffmpy_cmd_out(FFmpeg)
+    assert Path('subtitle-eng.vtt') == EncodeSubtitleStream(wrapper).execute(Path('input.mkv'), SubtitleStream(index=0, lang='eng'))
+    assert calls == ['ffmpeg -i input.mkv -map 0:0 subtitle-eng.vtt']
+
+def test_EncodeAudioStream():
+    wrapper, calls = ffmpy_cmd_out(FFmpeg)
+    assert Path('audio-eng.webm') == EncodeAudioStream(wrapper).execute(Path('input.mkv'), AudioStream(index=0, codec='dts', lang='eng'))
+    assert calls == ['ffmpeg -i input.mkv -map 0:0 -c:a libvorbis -b:a 160k -f webm -dash 1 audio-eng.webm']
+
+def test_EncodeVideoStream():
+    wrapper, calls = ffmpy_cmd_out(FFmpeg)
+    assert Path('video-1080p.webm') == EncodeVideoStream(1080, wrapper).execute(Path('input.mkv'), VideoStream(index=0, codec='h264', width=1920, height=1080))
+    assert calls == [
+        'ffmpeg -i input.mkv -map 0:0 -c:v libvpx-vp9 -s 1920x1080 -b:v 6M -tile-columns 6 -frame-parallel 1 -keyint_min 150 -g 150 -qmin 0 -qmax 50 -f webm -dash 1 -pass 1 -speed 4 /dev/null',
+        'ffmpeg -i input.mkv -map 0:0 -c:v libvpx-vp9 -s 1920x1080 -b:v 6M -tile-columns 6 -frame-parallel 1 -keyint_min 150 -g 150 -qmin 0 -qmax 50 -f webm -dash 1 -pass 2 -speed 1 -auto-alt-ref 1 -lag-in-frames 25 video-1080p.webm'
     ]
